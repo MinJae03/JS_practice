8d8268cff0136795b9cceeb0d14fc8a3dbfcce6f
MinJae03 <rhdfyd656@gmail.com>
2022-02-18 18:55:35 +0900

    변수 종류 알아봄

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 alert("hi");
+console.log(213124);
+console.log("hello");
+console.log('hello');
+
+console.log(5+2);
+console.log(5*2);
+console.log(5/2);
+
+
+const a = 5;
+console.log(a+2);
+console.log(a*2);
+console.log(a/2);
+
+const b = 10;
+console.log(b+2);
+console.log(b*2);
+console.log(b/2);
+
+let c = 10;
+console.log(c+2);
+console.log(c*2);
+console.log(c/2);
+
+let c = 5;
+let d = 2;
+console.log(c+d);
+console.log(c*d);
+console.log(c/d);
+
+var e = 8;
+var e = 11;
+console.log(e);
